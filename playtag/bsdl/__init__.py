7c038651cb5e4670343ee9b3a73651e651336123
pmaupin <pmaupin@c3674bfd-1efa-58b8-ce64-3efd99a46307>
2011-10-10 22:48:41 +0000

    Initial generation of part index from BSDL downloads

---
+++ playtag/bsdl/__init__.py
@@ -0,0 +1,10 @@
+'''
+This directory contains a minimal BSDL file parser that
+needs to be extended in order to be very useful.
+
+It also contains a data directory that has manufacturer's
+codes and known ID codes and instruction register capture
+codes for several parts.
+
+The tools to maintain this directory are in ../../tools/bsdl
+'''
