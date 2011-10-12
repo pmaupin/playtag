f2b09841e2edeabf4099fdf248ecf78fe059433e
pmaupin <pmaupin@c3674bfd-1efa-58b8-ce64-3efd99a46307>
2011-10-12 02:03:15 +0000

    Make it work when executed from a different directory

--- trunk/tools/jtag/discover.py
+++ trunk/tools/jtag/discover.py
@@ -4,7 +4,9 @@ Simplistic chain discovery
 '''
 
 import sys
-sys.path.append('../..')
+import os
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '../..'))
 
 from playtag.cables.digilent import Jtagger
 from playtag.jtag.discover import Chain
